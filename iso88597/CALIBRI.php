ee820cc1841e7a3624599e06d73c8c9af8451d69
Jorge Alves <jorge.brizio@gmail.com>
2017-05-16 22:25:43 +0200

    Adding fonts.

---
+++ iso88597/CALIBRI.php
@@ -0,0 +1,26 @@
+<?php
+$type = 'TrueType';
+$name = 'Calibri';
+$desc = array('Ascent'=>750,'Descent'=>-250,'CapHeight'=>632,'Flags'=>32,'FontBBox'=>'[-503 -307 1240 964]','ItalicAngle'=>0,'StemV'=>70,'MissingWidth'=>507);
+$up = -113;
+$ut = 65;
+$cw = array(
+	chr(0)=>507,chr(1)=>507,chr(2)=>507,chr(3)=>507,chr(4)=>507,chr(5)=>507,chr(6)=>507,chr(7)=>507,chr(8)=>507,chr(9)=>507,chr(10)=>507,chr(11)=>507,chr(12)=>507,chr(13)=>507,chr(14)=>507,chr(15)=>507,chr(16)=>507,chr(17)=>507,chr(18)=>507,chr(19)=>507,chr(20)=>507,chr(21)=>507,
+	chr(22)=>507,chr(23)=>507,chr(24)=>507,chr(25)=>507,chr(26)=>507,chr(27)=>507,chr(28)=>507,chr(29)=>507,chr(30)=>507,chr(31)=>507,' '=>226,'!'=>326,'"'=>401,'#'=>498,'$'=>507,'%'=>715,'&'=>682,'\''=>221,'('=>303,')'=>303,'*'=>498,'+'=>498,
+	','=>250,'-'=>306,'.'=>252,'/'=>386,'0'=>507,'1'=>507,'2'=>507,'3'=>507,'4'=>507,'5'=>507,'6'=>507,'7'=>507,'8'=>507,'9'=>507,':'=>268,';'=>268,'<'=>498,'='=>498,'>'=>498,'?'=>463,'@'=>894,'A'=>579,
+	'B'=>544,'C'=>533,'D'=>615,'E'=>488,'F'=>459,'G'=>631,'H'=>623,'I'=>252,'J'=>319,'K'=>520,'L'=>420,'M'=>855,'N'=>646,'O'=>662,'P'=>517,'Q'=>673,'R'=>543,'S'=>459,'T'=>487,'U'=>642,'V'=>567,'W'=>890,
+	'X'=>519,'Y'=>487,'Z'=>468,'['=>307,'\\'=>386,']'=>307,'^'=>498,'_'=>498,'`'=>291,'a'=>479,'b'=>525,'c'=>423,'d'=>525,'e'=>498,'f'=>305,'g'=>471,'h'=>525,'i'=>229,'j'=>239,'k'=>455,'l'=>229,'m'=>799,
+	'n'=>525,'o'=>527,'p'=>525,'q'=>525,'r'=>349,'s'=>391,'t'=>335,'u'=>525,'v'=>452,'w'=>715,'x'=>433,'y'=>453,'z'=>395,'{'=>314,'|'=>460,'}'=>314,'~'=>498,chr(127)=>507,chr(128)=>507,chr(129)=>507,chr(130)=>507,chr(131)=>507,
+	chr(132)=>507,chr(133)=>507,chr(134)=>507,chr(135)=>507,chr(136)=>507,chr(137)=>507,chr(138)=>507,chr(139)=>507,chr(140)=>507,chr(141)=>507,chr(142)=>507,chr(143)=>507,chr(144)=>507,chr(145)=>507,chr(146)=>507,chr(147)=>507,chr(148)=>507,chr(149)=>507,chr(150)=>507,chr(151)=>507,chr(152)=>507,chr(153)=>507,
+	chr(154)=>507,chr(155)=>507,chr(156)=>507,chr(157)=>507,chr(158)=>507,chr(159)=>507,chr(160)=>226,chr(161)=>250,chr(162)=>250,chr(163)=>507,chr(164)=>507,chr(165)=>507,chr(166)=>498,chr(167)=>498,chr(168)=>393,chr(169)=>834,chr(170)=>507,chr(171)=>512,chr(172)=>498,chr(173)=>306,chr(174)=>507,chr(175)=>905,
+	chr(176)=>339,chr(177)=>498,chr(178)=>336,chr(179)=>334,chr(180)=>317,chr(181)=>494,chr(182)=>579,chr(183)=>252,chr(184)=>488,chr(185)=>623,chr(186)=>252,chr(187)=>512,chr(188)=>662,chr(189)=>671,chr(190)=>487,chr(191)=>664,chr(192)=>274,chr(193)=>579,chr(194)=>544,chr(195)=>416,chr(196)=>564,chr(197)=>488,
+	chr(198)=>468,chr(199)=>623,chr(200)=>662,chr(201)=>252,chr(202)=>520,chr(203)=>573,chr(204)=>855,chr(205)=>646,chr(206)=>492,chr(207)=>662,chr(208)=>623,chr(209)=>517,chr(210)=>507,chr(211)=>458,chr(212)=>487,chr(213)=>487,chr(214)=>759,chr(215)=>519,chr(216)=>750,chr(217)=>664,chr(218)=>252,chr(219)=>487,
+	chr(220)=>567,chr(221)=>456,chr(222)=>537,chr(223)=>274,chr(224)=>542,chr(225)=>567,chr(226)=>531,chr(227)=>446,chr(228)=>523,chr(229)=>456,chr(230)=>348,chr(231)=>537,chr(232)=>532,chr(233)=>274,chr(234)=>455,chr(235)=>463,chr(236)=>550,chr(237)=>449,chr(238)=>376,chr(239)=>527,chr(240)=>553,chr(241)=>509,
+	chr(242)=>411,chr(243)=>532,chr(244)=>387,chr(245)=>542,chr(246)=>651,chr(247)=>426,chr(248)=>708,chr(249)=>696,chr(250)=>274,chr(251)=>542,chr(252)=>527,chr(253)=>542,chr(254)=>696,chr(255)=>507);
+$enc = 'ISO-8859-7';
+$diff = '128 /.notdef 130 /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef 142 /.notdef 145 /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef 158 /.notdef /.notdef 161 /quoteleft /quoteright 164 /.notdef /.notdef 170 /.notdef 174 /.notdef /afii00208 180 /tonos /dieresistonos /Alphatonos 184 /Epsilontonos /Etatonos /Iotatonos 188 /Omicrontonos 190 /Upsilontonos /Omegatonos /iotadieresistonos /Alpha /Beta /Gamma /Delta /Epsilon /Zeta /Eta /Theta /Iota /Kappa /Lambda /Mu /Nu /Xi /Omicron /Pi /Rho /.notdef /Sigma /Tau /Upsilon /Phi /Chi /Psi /Omega /Iotadieresis /Upsilondieresis /alphatonos /epsilontonos /etatonos /iotatonos /upsilondieresistonos /alpha /beta /gamma /delta /epsilon /zeta /eta /theta /iota /kappa /lambda /mu /nu /xi /omicron /pi /rho /sigma1 /sigma /tau /upsilon /phi /chi /psi /omega /iotadieresis /upsilondieresis /omicrontonos /upsilontonos /omegatonos /.notdef';
+$uv = array(0=>array(0,161),161=>array(8216,2),163=>163,166=>array(166,4),171=>array(171,3),175=>8213,176=>array(176,4),180=>array(900,3),183=>183,184=>array(904,3),187=>187,188=>908,189=>189,190=>array(910,20),211=>array(931,44));
+$file = 'CALIBRI.z';
+$originalsize = 55996;
+$subsetted = true;
+?>
---
+++ iso88597/CALIBRIB.php
@@ -0,0 +1,26 @@
+<?php
+$type = 'TrueType';
+$name = 'Calibri-Bold';
+$desc = array('Ascent'=>750,'Descent'=>-250,'CapHeight'=>632,'Flags'=>32,'FontBBox'=>'[-519 -306 1240 971]','ItalicAngle'=>0,'StemV'=>120,'MissingWidth'=>507);
+$up = -87;
+$ut = 91;
+$cw = array(
+	chr(0)=>507,chr(1)=>507,chr(2)=>507,chr(3)=>507,chr(4)=>507,chr(5)=>507,chr(6)=>507,chr(7)=>507,chr(8)=>507,chr(9)=>507,chr(10)=>507,chr(11)=>507,chr(12)=>507,chr(13)=>507,chr(14)=>507,chr(15)=>507,chr(16)=>507,chr(17)=>507,chr(18)=>507,chr(19)=>507,chr(20)=>507,chr(21)=>507,
+	chr(22)=>507,chr(23)=>507,chr(24)=>507,chr(25)=>507,chr(26)=>507,chr(27)=>507,chr(28)=>507,chr(29)=>507,chr(30)=>507,chr(31)=>507,' '=>226,'!'=>326,'"'=>438,'#'=>498,'$'=>507,'%'=>729,'&'=>705,'\''=>233,'('=>312,')'=>312,'*'=>498,'+'=>498,
+	','=>258,'-'=>306,'.'=>267,'/'=>430,'0'=>507,'1'=>507,'2'=>507,'3'=>507,'4'=>507,'5'=>507,'6'=>507,'7'=>507,'8'=>507,'9'=>507,':'=>276,';'=>276,'<'=>498,'='=>498,'>'=>498,'?'=>463,'@'=>898,'A'=>606,
+	'B'=>561,'C'=>529,'D'=>630,'E'=>488,'F'=>459,'G'=>637,'H'=>631,'I'=>267,'J'=>331,'K'=>547,'L'=>423,'M'=>874,'N'=>659,'O'=>676,'P'=>532,'Q'=>686,'R'=>563,'S'=>473,'T'=>495,'U'=>653,'V'=>591,'W'=>906,
+	'X'=>551,'Y'=>520,'Z'=>478,'['=>325,'\\'=>430,']'=>325,'^'=>498,'_'=>498,'`'=>300,'a'=>494,'b'=>537,'c'=>418,'d'=>537,'e'=>503,'f'=>316,'g'=>474,'h'=>537,'i'=>246,'j'=>255,'k'=>480,'l'=>246,'m'=>813,
+	'n'=>537,'o'=>538,'p'=>537,'q'=>537,'r'=>355,'s'=>399,'t'=>347,'u'=>537,'v'=>473,'w'=>745,'x'=>459,'y'=>474,'z'=>397,'{'=>344,'|'=>475,'}'=>344,'~'=>498,chr(127)=>507,chr(128)=>507,chr(129)=>507,chr(130)=>507,chr(131)=>507,
+	chr(132)=>507,chr(133)=>507,chr(134)=>507,chr(135)=>507,chr(136)=>507,chr(137)=>507,chr(138)=>507,chr(139)=>507,chr(140)=>507,chr(141)=>507,chr(142)=>507,chr(143)=>507,chr(144)=>507,chr(145)=>507,chr(146)=>507,chr(147)=>507,chr(148)=>507,chr(149)=>507,chr(150)=>507,chr(151)=>507,chr(152)=>507,chr(153)=>507,
+	chr(154)=>507,chr(155)=>507,chr(156)=>507,chr(157)=>507,chr(158)=>507,chr(159)=>507,chr(160)=>226,chr(161)=>258,chr(162)=>258,chr(163)=>507,chr(164)=>507,chr(165)=>507,chr(166)=>498,chr(167)=>498,chr(168)=>415,chr(169)=>834,chr(170)=>507,chr(171)=>539,chr(172)=>498,chr(173)=>306,chr(174)=>507,chr(175)=>905,
+	chr(176)=>342,chr(177)=>498,chr(178)=>338,chr(179)=>336,chr(180)=>317,chr(181)=>494,chr(182)=>606,chr(183)=>268,chr(184)=>488,chr(185)=>631,chr(186)=>267,chr(187)=>539,chr(188)=>676,chr(189)=>691,chr(190)=>520,chr(191)=>681,chr(192)=>286,chr(193)=>606,chr(194)=>561,chr(195)=>410,chr(196)=>578,chr(197)=>488,
+	chr(198)=>478,chr(199)=>631,chr(200)=>676,chr(201)=>267,chr(202)=>547,chr(203)=>594,chr(204)=>874,chr(205)=>659,chr(206)=>492,chr(207)=>676,chr(208)=>631,chr(209)=>532,chr(210)=>507,chr(211)=>470,chr(212)=>495,chr(213)=>520,chr(214)=>801,chr(215)=>551,chr(216)=>808,chr(217)=>681,chr(218)=>267,chr(219)=>520,
+	chr(220)=>591,chr(221)=>458,chr(222)=>544,chr(223)=>286,chr(224)=>548,chr(225)=>591,chr(226)=>545,chr(227)=>467,chr(228)=>533,chr(229)=>458,chr(230)=>364,chr(231)=>544,chr(232)=>545,chr(233)=>286,chr(234)=>480,chr(235)=>486,chr(236)=>563,chr(237)=>471,chr(238)=>385,chr(239)=>538,chr(240)=>579,chr(241)=>530,
+	chr(242)=>407,chr(243)=>544,chr(244)=>393,chr(245)=>548,chr(246)=>699,chr(247)=>451,chr(248)=>738,chr(249)=>718,chr(250)=>286,chr(251)=>548,chr(252)=>538,chr(253)=>548,chr(254)=>718,chr(255)=>507);
+$enc = 'ISO-8859-7';
+$diff = '128 /.notdef 130 /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef 142 /.notdef 145 /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef /.notdef 158 /.notdef /.notdef 161 /quoteleft /quoteright 164 /.notdef /.notdef 170 /.notdef 174 /.notdef /afii00208 180 /tonos /dieresistonos /Alphatonos 184 /Epsilontonos /Etatonos /Iotatonos 188 /Omicrontonos 190 /Upsilontonos /Omegatonos /iotadieresistonos /Alpha /Beta /Gamma /Delta /Epsilon /Zeta /Eta /Theta /Iota /Kappa /Lambda /Mu /Nu /Xi /Omicron /Pi /Rho /.notdef /Sigma /Tau /Upsilon /Phi /Chi /Psi /Omega /Iotadieresis /Upsilondieresis /alphatonos /epsilontonos /etatonos /iotatonos /upsilondieresistonos /alpha /beta /gamma /delta /epsilon /zeta /eta /theta /iota /kappa /lambda /mu /nu /xi /omicron /pi /rho /sigma1 /sigma /tau /upsilon /phi /chi /psi /omega /iotadieresis /upsilondieresis /omicrontonos /upsilontonos /omegatonos /.notdef';
+$uv = array(0=>array(0,161),161=>array(8216,2),163=>163,166=>array(166,4),171=>array(171,3),175=>8213,176=>array(176,4),180=>array(900,3),183=>183,184=>array(904,3),187=>187,188=>908,189=>189,190=>array(910,20),211=>array(931,44));
+$file = 'CALIBRIB.z';
+$originalsize = 53812;
+$subsetted = true;
+?>
